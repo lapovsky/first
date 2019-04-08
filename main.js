87b8a8dd2722a218654a644a232cb60503647bdb
łapa <mr.laapaa@gmail.com>
2019-04-08 12:38:30 +0200

    Add button (change background color (normal/white))

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ const addDot = setInterval(function(){
         number = 0;
     }
 },1000);
+    let whiteFlag = false;
+document.querySelector('button').addEventListener('click',function(){
+    if (whiteFlag === false){
+        document.body.style.backgroundColor = 'white';
+        whiteFlag = true;
+    }
+    else if(whiteFlag === true){
+        document.body.style.backgroundColor = 'rgba(10,20,30,1)';
+        whiteFlag = false;
+    }
+})
